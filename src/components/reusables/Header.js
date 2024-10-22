6ed716269439957a4adc2a759f3f07b663a1b53a
Thibault Boutaud <krom@gmx.fr>
2024-10-22 10:02:32 +0200

    modification page accueil

--- src/components/reusables/Header.js
+++ src/components/reusables/Header.js
@@ -15,13 +15,13 @@ const Header = () => {
     return (
         <header>
             <div className="header__left">
-                <NavLink className="nav__iconContainer bgBlue" to="/">
-                    <i className="fa-solid fa-igloo squareIcon bgGreen"></i>
+                <NavLink className="nav__iconContainer" to="/">
+                <img src={logoUk} alt="logo uk" />
                 </NavLink>
             </div>
             <div className="header__right">
                 <div className="header__right__logoArea">
-                    <img src={logoUk} alt="logo uk" />
+                
                     <p>Envie d'apprendre du vocabulaire ?</p>
                 </div>
                 <div className="header__right__profilArea">
